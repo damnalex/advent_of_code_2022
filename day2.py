16ad966cfa5078e9fa6203ec159a9922fba83d64
Moens Alexandre <mao@odoo.com>
2022-12-04 17:48:01 +0100

    [DAY 2] part 2

--- day2.py
+++ day2.py
@@ -65,3 +65,39 @@ def count_points(input):
 input_file = "day2_input.txt"
 with open(input_file, 'r') as f:
     print(count_points(f))
+
+
+
+"""
+--- Part Two ---
+The Elf finishes helping with the tent and sneaks back over to you. "Anyway, the second column says how the round needs to end: X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win. Good luck!"
+
+The total score is still calculated in the same way, but now you need to figure out what shape to choose so the round ends as indicated. The example above now goes like this:
+
+In the first round, your opponent will choose Rock (A), and you need the round to end in a draw (Y), so you also choose Rock. This gives you a score of 1 + 3 = 4.
+In the second round, your opponent will choose Paper (B), and you choose Rock so you lose (X) with a score of 1 + 0 = 1.
+In the third round, you will defeat your opponent's Scissors with Rock for a score of 1 + 6 = 7.
+Now that you're correctly decrypting the ultra top secret strategy guide, you would get a total score of 12.
+
+Following the Elf's instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide?
+"""
+conversion_map = {
+    "A": "CAB",
+    "B": "ABC",
+    "C": "BCA"
+}
+
+picker = {
+    "X": 0, "Y": 1, "Z": 2
+}
+
+def convert_input(input):
+    for line in input:
+        their_letter, result = line.split()
+        my_letter = conversion_map[their_letter][picker[result]]
+        yield f"{their_letter} {my_letter}"
+
+
+input_file = "day2_input.txt"
+with open(input_file, 'r') as f:
+    print(count_points(convert_input(f)))
