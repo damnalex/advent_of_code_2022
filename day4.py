9906b1cd42682682bdc01e90419fdbf059f5d27d
Moens Alexandre <mao@odoo.com>
2022-12-04 18:32:39 +0100

    [DAY 4] part 2

--- day4.py
+++ day4.py
@@ -69,3 +69,37 @@ input_file = "day4_input.txt"
 with open(input_file, 'r') as f:
     c_input = clean_input(f)
     print(count_overlaps(c_input))
+
+
+""" --- Part Two ---
+It seems like there is still quite a bit of duplicate work planned. Instead, the Elves would like to know the number of pairs that overlap at all.
+
+In the above example, the first two pairs (2-4,6-8 and 2-3,4-5) don't overlap, while the remaining four pairs (5-7,7-9, 2-8,3-7, 6-6,4-6, and 2-6,4-8) do overlap:
+
+5-7,7-9 overlaps in a single section, 7.
+2-8,3-7 overlaps all of the sections 3 through 7.
+6-6,4-6 overlaps in a single section, 6.
+2-6,4-8 overlaps in sections 4, 5, and 6.
+So, in this example, the number of overlapping assignment pairs is 4.
+
+In how many assignment pairs do the ranges overlap?
+"""
+
+def is_any_overlapping(line):
+    ranges = line.split(',')
+    borns = [range.split("-") for range in ranges]
+    a1, a2, b1, b2  = [int(i) for i in chain(*borns)]
+    b = [(a1,a2),(b1, b2)]
+    b.sort()
+    return b[0][0] <= b[1][0] <= b[0][1]
+
+def count_any_overlaps(input):
+    return sum(
+        int(is_any_overlapping(line))
+        for line in input
+    )
+
+input_file = "day4_input.txt"
+with open(input_file, 'r') as f:
+    c_input = clean_input(f)
+    print(count_any_overlaps(c_input))
